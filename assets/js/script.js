606781fa9534b0e971fb0f1bbca512c1d044c74d
Shirly <shirlyd@axioned.com>
2021-08-15 15:41:48 +0530

    js commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,23 @@
-/* Author: 
 
-*/
+var box = document.querySelectorAll('.content-box');
+for (var i = 0; i < box.length; i++) {
+    box[i].addEventListener('click', function(e){
+        var boxElem = e.target.getAttribute('src');
+        lightBox(boxElem);
+
+         
+        console.log(this, e.target);
+        
+    })
+}
+
+function lightBox(box) {
+    var lightBox = document.querySelector('.box');
+    lightBox.classList.add('active');   
+    var elem = document.querySelector('.light-box');
+    console.log(elem);
+    elem.setAttribute("src", box);
+}
 
 
 
