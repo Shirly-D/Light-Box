d89f80bfb847caa9b94b8f14fd2284a4a7edd193
Shirly <shirlyd@axioned.com>
2021-08-15 19:04:22 +0530

    js function final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,8 @@
-
 var box = document.querySelectorAll('.content-box');
 for (var i = 0; i < box.length; i++) {
     box[i].addEventListener('click', function(e){
         var boxElem = e.target.getAttribute('src');
         lightBox(boxElem);
-
-         
-        console.log(this, e.target);
-        
     })
 }
 
@@ -15,8 +10,10 @@ function lightBox(box) {
     var lightBox = document.querySelector('.box');
     lightBox.classList.add('active');   
     var elem = document.querySelector('.image-box');
-    console.log(elem);
     elem.setAttribute("src", box);
+    lightBox.addEventListener('click', function(e){
+        lightBox.classList.remove('active');
+    })
 }
 
 
