65d0bf646d4f3581b6582669342a9624a4116e50
Shirly <shirlyd@axioned.com>
2021-08-16 00:41:37 +0530

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,18 @@
 var box = document.querySelectorAll('.content-box');
 for (var i = 0; i < box.length; i++) {
     box[i].addEventListener('click', function(e){
-        var boxElem = e.target.getAttribute('src');
+        var boxElem = e.currentTarget.getAttribute('data-image');
+        var text = e.currentTarget.getAttribute('data-text');
         lightBox(boxElem);
     })
 }
 
-function lightBox(box) {
+function lightBox(box, boxText) {
     var lightBox = document.querySelector('.box');
     lightBox.classList.add('active');   
     var elem = document.querySelector('.image-box');
     elem.setAttribute("src", box);
+
     lightBox.addEventListener('click', function(e){
         lightBox.classList.remove('active');
     })
